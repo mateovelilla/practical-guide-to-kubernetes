2cf0e25b9705e9c7621423290e1319f2eb60b6c9
mateovelilla <teovelilla@gmail.com>
2025-05-25 19:50:59 -0500

    chore: create option to build a simple go app

---
+++ src/handlers/pods/simpleAPIMongoDB/index.ts
@@ -0,0 +1,28 @@
+import { readFileSync, writeFileSync } from "node:fs"
+import { dirname } from "node:path"
+import { fileURLToPath } from 'url';
+import { spinner, log, text } from "@clack/prompts";
+import { AbstractHandler } from "../../handler.abstract.ts"
+import { type Request } from "../../request.type.ts"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export class SimpleApiMongoHandler extends AbstractHandler {
+    public async handle(request: Request) {
+        try {
+            const loader = spinner()
+            loader.start(`Creating stack!`);
+            const { stdout: outputKubectl, stderr: outputKubectlError } =  await super.runCommand("kubectl",["create", "-f", `${__dirname}/simple-api-mongo.yml`])
+            log.info(outputKubectl);
+            log.warn(outputKubectlError)
+            loader.stop("")
+            const { stdout: outputGetPods, stderr: outputGetPodsError } =  await super.runCommand("kubectl",["get", "pods"])
+            log.info(outputGetPods);
+            log.warn(outputGetPodsError)
+        } catch (error) {
+            console.log(error)
+        }finally {
+            return await super.handle(request)
+        }
+       
+    }
+}
--- src/proxies/pods.ts
+++ src/proxies/pods.ts
@@ -1,5 +1,6 @@
 import { spinner, log, select } from "@clack/prompts";
 import { SimpleMongoPodHandler } from "../handlers/pods/simpleMongoPod/index.ts"
+import { SimpleApiMongoHandler } from "../handlers/pods/simpleAPIMongoDB/index.ts"
 import type { AbstractHandler } from "../handlers/handler.abstract.ts";
 export async function run(){
     let handler: AbstractHandler
@@ -7,6 +8,7 @@ export async function run(){
         message: 'What project do you want to implement?',
         options: [
             { value: 'simple-mongo-pod', label: 'Simple Pod with MongoDB', hint: "This Kubernetes manifest defines a single Pod named db that runs a MongoDB container using the mongo:3.3 image. The container is configured to run the mongod command with two arguments: --rest and --httpinterface, which enable REST access and the HTTP interface for the MongoDB server. The Pod is labeled with metadata to indicate its type (db) and vendor (MongoLabs), which can be useful for selection or organization within a cluster." },
+            { value: 'simple-api-mongo-pod', label: 'Simple Pods with API in Go and  MongoDB', hint: "A MongoDB container (mongo:3.3) acting as the database and a Go-based API container (vfarcic/go-demo-2) that connects to the MongoDB using localhost." },    
             { value: 'Other', label: 'nothing' },
         ],
     });
@@ -14,7 +16,9 @@ export async function run(){
         case "simple-mongo-pod":
             handler = new SimpleMongoPodHandler();
             break;
-    
+        case "simple-api-mongo-pod":
+            handler = new SimpleApiMongoHandler();
+            break;
         default:
             break;
     }
