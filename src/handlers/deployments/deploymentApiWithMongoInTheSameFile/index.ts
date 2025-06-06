64ee83500ef634f8d4de1d6fcf1764846bb58e0d
mateovelilla <teovelilla@gmail.com>
2025-06-06 06:41:54 -0500

    chore: finished the last example in the deployment options

---
+++ src/handlers/deployments/deploymentApiWithMongoInTheSameFile/index.ts
@@ -0,0 +1,24 @@
+import { dirname } from "node:path"
+import { fileURLToPath } from 'url';
+import { spinner, log } from "@clack/prompts";
+import { AbstractHandler } from "../../handler.abstract.ts"
+import { type Request } from "../../request.type.ts"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export class DeploymentApiWithMongoInTheSameFileTimeHandler extends AbstractHandler {
+    public async handle(request: Request) {
+        try {
+            const loader = spinner()
+            loader.start(`Creating Deployments!`);
+            const { stdout: outputKubectl, stderr: outputKubectlError } =  await super.runCommand("kubectl",["create", "-f", `${__dirname}/go-demo-2.yml`])
+            log.info(outputKubectl);
+            log.warn(outputKubectlError)
+            loader.stop("____________________________")
+        } catch (error) {
+            console.log(error)
+        }finally {
+            return await super.handle(request)
+        }
+       
+    }
+}
--- src/proxies/deployments.ts
+++ src/proxies/deployments.ts
@@ -1,8 +1,9 @@
 import { select } from "@clack/prompts";
+import type { AbstractHandler } from "../handlers/handler.abstract.ts";
 import { SimpleMongoDeploymentHandler } from "../handlers/deployments/simpleMongoDeployment/index.ts"
 import { MongoDeploymentWithServiceHandler } from "../handlers/deployments/deploymentMongoDbWithService/index.ts"
 import { DeploymentWithZeroDownTimeHandler } from "../handlers/deployments/deploymentZeroDownTime/index.ts";
-import type { AbstractHandler } from "../handlers/handler.abstract.ts";
+import { DeploymentApiWithMongoInTheSameFileTimeHandler } from "../handlers/deployments/deploymentApiWithMongoInTheSameFile/index.ts"; 
 import { StatusPodsCheckerHandler } from "../handlers/utils/statusPodsChecker/index.ts";
 import { StatusDeploymentsCheckerHandler } from "../handlers/utils/statusDeploymentsChecker/index.ts";
 import { ListPodsHandler } from "../handlers/utils/listPods/index.ts";
@@ -14,7 +15,7 @@ export async function run(){
             { value: 'simple-mongo-deployment', label: 'Simple Deployment with MongoDB', hint: "defines a Deployment named go-demo-2-db that manages a MongoDB database service. It uses the apps/v1 API version and creates Pods with a container running the mongo:3.3 image. The Deployment uses a selector to match Pods labeled with type: db and service: go-demo-2. Each Pod will include these labels, along with an additional vendor: MongoLabs label. The MongoDB container inside the Pods exposes port 28017. Essentially, this manifest sets up a database backend (MongoDB) as a replicated and self-healing service within Kubernetes, ensuring that the database service is consistently available." },
             { value: 'mongo-deployment-with-service', label: 'Deployment with MongoDB with service', hint: "defines a Deployment named go-demo-2-db that manages a MongoDB database service. It uses the apps/v1 API version and creates Pods with a container running the mongo:3.3 image. The Deployment uses a selector to match Pods labeled with type: db and service: go-demo-2. Each Pod will include these labels, along with an additional vendor: MongoLabs label. The MongoDB container inside the Pods exposes port 28017. Essentially, this manifest sets up a database backend (MongoDB) as a replicated and self-healing service within Kubernetes, ensuring that the database service is consistently available and finally create a service associate to the deployment." },
             { value: 'deployment-zero-downtime', label: 'Deployment with zero downtime', hint: "This Kubernetes Deployment manifest defines a service called go-demo-2-api that maintains 3 replicas of a container running the vfarcic/go-demo-2 image. It uses a RollingUpdate strategy with a max surge and max unavailable of 1 to ensure smooth, zero-downtime updates. Pods are labeled with type: api, service: go-demo-2, and language: go, and matched by the selector to be managed by this deployment. Each container sets the environment variable DB=go-demo-2-db and exposes a health check endpoint at /demo/hello on port 8080, used for both readiness and liveness probes. The deployment includes rollout controls like minReadySeconds: 1, progressDeadlineSeconds: 60, and a revisionHistoryLimit of 5 to manage stability and rollback capabilities." },
-            { value: 'Other', label: 'nothing' },
+            { value: 'deployment-zero-downtime-in-the-same-file', label: 'Deployment of an API and MongoDb with zero downtime in the same manifest', hint: "This Kubernetes Deployment manifest defines a service called go-demo-2-api that maintains 3 replicas of a container running the vfarcic/go-demo-2 image. It uses a RollingUpdate strategy with a max surge and max unavailable of 1 to ensure smooth, zero-downtime updates. Pods are labeled with type: api, service: go-demo-2, and language: go, and matched by the selector to be managed by this deployment. Each container sets the environment variable DB=go-demo-2-db and exposes a health check endpoint at /demo/hello on port 8080, used for both readiness and liveness probes. The deployment includes rollout controls like minReadySeconds: 1, progressDeadlineSeconds: 60, and a revisionHistoryLimit of 5 to manage stability and rollback capabilities in the same file." },
         ],
     });
     const statusCheckerPods = new StatusPodsCheckerHandler()
@@ -45,6 +46,12 @@ export async function run(){
              statusCheckerPods.setNext(statusCheckerDeployments)
              mongoDeploymentWithService.setNext(statusCheckerPods)
              handler.setNext(mongoDeploymentWithService)
+            break;
+        case "deployment-zero-downtime-in-the-same-file":
+            handler = new DeploymentApiWithMongoInTheSameFileTimeHandler();
+            statusCheckerDeployments.setNext(listPods)
+            statusCheckerPods.setNext(statusCheckerDeployments)
+            handler.setNext(statusCheckerPods)
             break;
         default:
             break;
