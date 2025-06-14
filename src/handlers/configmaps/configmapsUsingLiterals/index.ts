917510dfbcfc93fdab5035d736ac3e90ca279e48
mateovelilla <teovelilla@gmail.com>
2025-06-14 09:40:28 -0500

    chore: create new configmap option using literals

--- src/handlers/configmaps/configmapsUsingLiterals/index.ts
+++ src/handlers/configmaps/configmapsUsingLiterals/index.ts
@@ -6,16 +6,16 @@ import { type Request } from "../../request.type.ts"
 import { StatusPodsCheckerHandler } from "../../utils/statusPodsChecker/index.ts"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-export class SimpleConfigmapWithFilesSharedHandler extends AbstractHandler {
+export class ConfigmapsUsingLiteralsHandler extends AbstractHandler {
     public async handle(request: Request) {
         try {
             const loader = spinner()
             const statusPodsChecker = new StatusPodsCheckerHandler()
             loader.start(`Creating Creating Config!`);
-            const { stdout: outputConfigmap, stderr: outputConfigmapError } =  await super.runCommand("kubectl",["create", "cm", "my-config",`--from-file=${__dirname}/prometheus-conf.yml`])
+            const { stdout: outputConfigmap, stderr: outputConfigmapError } =  await super.runCommand("kubectl",["create", "cm", "my-config",`--from-literal=something=else`, `--from-literal=wather=sunny`])
             log.info(outputConfigmap);
             log.warn(outputConfigmapError);
-            const { stdout: outputDescribeConfigmap, stderr: outputDescribeConfigmapError } =  await super.runCommand("kubectl",["describe", "cm", "my-config"])
+            const { stdout: outputDescribeConfigmap, stderr: outputDescribeConfigmapError } =  await super.runCommand("kubectl",["get", "cm", "my-config", "-o", "yaml"])
             log.info(outputDescribeConfigmap);
             log.warn(outputDescribeConfigmapError);
             loader.stop();
@@ -29,6 +29,10 @@ export class SimpleConfigmapWithFilesSharedHandler extends AbstractHandler {
             const { stdout: outputExecutedCommand, stderr: outputExecutedCommandError } =  await super.runCommand("kubectl",["exec", "alpine",'--','ls','/etc/config']);
             log.info(outputExecutedCommand);
             log.warn(outputExecutedCommandError);
+            log.info("Listing content something file");
+            const { stdout: outputDescribeCommand, stderr: outputDescribeCommandError } =  await super.runCommand("kubectl",["exec", "alpine",'--','cat','/etc/config/something']);
+            log.info(outputDescribeCommand);
+            log.warn(outputDescribeCommandError);
             loader.stop("____________________________");
         } catch (error) {
             console.log(error)
--- src/proxies/configmaps.ts
+++ src/proxies/configmaps.ts
@@ -1,17 +1,23 @@
 import { select } from "@clack/prompts";
 import type { AbstractHandler } from "../handlers/handler.abstract.ts";
 import { SimpleConfigmapWithFilesSharedHandler } from "../handlers/configmaps/simpleConfigMapWithFilesShared/index.ts"
+import { ConfigmapsUsingLiteralsHandler } from "../handlers/configmaps/configmapsUsingLiterals/index.ts"
 export async function run(){
     const podProject = await select({
         message: 'What project do you want to implement?',
         options: [
             { value: 'simple-configmap-with-files-shared', label: 'Simple shared in alpine pod', hint: "This Kubernetes manifest defines a simple Pod named alpine that runs a single container based on the lightweight Alpine Linux image. The container executes the sleep 100000 command to stay alive for testing or debugging purposes. A volume named config-vol is mounted inside the container at the path /etc/config, and this volume is sourced from a ConfigMap named my-config. This setup allows the container to access configuration data stored in the ConfigMap as files within the specified directory." },
+            { value: 'configmaps-using-literals', label: 'Configmaps using literals', hint: "This option allows you to generate a lightweight Alpine-based Pod that mounts a specific folder containing configuration files provided through ConfigMaps created from literal values. It’s particularly useful for testing or deploying minimal containers where configuration data needs to be injected at runtime in a structured and accessible way within the container’s filesystem." },
+
         ],
     });
     let handler: AbstractHandler
     switch (podProject) {
         case "simple-configmap-with-files-shared":
             handler = new SimpleConfigmapWithFilesSharedHandler();
+            break;
+        case "configmaps-using-literals":
+            handler = new ConfigmapsUsingLiteralsHandler(); 
             break;
         default:
             break;
