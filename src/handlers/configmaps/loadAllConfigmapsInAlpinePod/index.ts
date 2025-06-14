8e3760020741b2a7afaed3ffa985947306fe6b4c
mateovelilla <teovelilla@gmail.com>
2025-06-14 11:09:44 -0500

    chore: new option to generate pods with environment variables charge from yml file

---
+++ src/handlers/configmaps/loadAllConfigmapsInAlpinePod/index.ts
@@ -0,0 +1,40 @@
+import { dirname } from "node:path"
+import { fileURLToPath } from 'url';
+import { spinner, log } from "@clack/prompts";
+import { AbstractHandler } from "../../handler.abstract.ts"
+import { type Request } from "../../request.type.ts"
+import { StatusPodsCheckerHandler } from "../../utils/statusPodsChecker/index.ts"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+export class LoadAllConfigmapsInAlpinePodHandler extends AbstractHandler {
+    public async handle(request: Request) {
+        try {
+            const loader = spinner()
+            const statusPodsChecker = new StatusPodsCheckerHandler()
+            loader.start(`Creating Creating Config!`);
+            const { stdout: outputConfigmap, stderr: outputConfigmapError } =  await super.runCommand("kubectl",["create", "cm", "my-config",`--from-env-file=${__dirname}/my-env-file.yml`])
+            log.info(outputConfigmap);
+            log.warn(outputConfigmapError);
+            const { stdout: outputDescribeConfigmap, stderr: outputDescribeConfigmapError } =  await super.runCommand("kubectl",["describe", "cm", "my-config"])
+            log.info(outputDescribeConfigmap);
+            log.warn(outputDescribeConfigmapError);
+            loader.stop();
+            loader.start(`Creating Alpine pod`);
+            const { stdout: outputAlpinePod, stderr: outputAlpinePodError } =  await super.runCommand("kubectl",["create", "-f", `${__dirname}/alpine.yml`])
+            log.info(outputAlpinePod);
+            log.warn(outputAlpinePodError);
+            loader.stop();
+            await statusPodsChecker.handle(request);
+            loader.start("Listing environment variables");
+            const { stdout: outputExecutedCommand, stderr: outputExecutedCommandError } =  await super.runCommand("kubectl",["exec", "alpine-env",'--','env']);
+            log.info(outputExecutedCommand);
+            log.warn(outputExecutedCommandError);
+            loader.stop("____________________________");
+        } catch (error) {
+            console.log(error)
+        }finally {
+            return await super.handle(request)
+        }
+       
+    }
+}
--- src/proxies/configmaps.ts
+++ src/proxies/configmaps.ts
@@ -3,6 +3,7 @@ import type { AbstractHandler } from "../handlers/handler.abstract.ts";
 import { SimpleConfigmapWithFilesSharedHandler } from "../handlers/configmaps/simpleConfigMapWithFilesShared/index.ts"
 import { ConfigmapsUsingLiteralsHandler } from "../handlers/configmaps/configmapsUsingLiterals/index.ts"
 import {  PodAlpineUsingEnvironmentVariablesHandler } from "../handlers/configmaps/podAlpineUsingEnvironmentVariables/index.ts"
+import { LoadAllConfigmapsInAlpinePodHandler } from "../handlers/configmaps/loadAllConfigmapsInAlpinePod/index.ts"
 export async function run(){
     const podProject = await select({
         message: 'What project do you want to implement?',
@@ -10,6 +11,7 @@ export async function run(){
             { value: 'simple-configmap-with-files-shared', label: 'Simple shared in alpine pod', hint: "This Kubernetes manifest defines a simple Pod named alpine that runs a single container based on the lightweight Alpine Linux image. The container executes the sleep 100000 command to stay alive for testing or debugging purposes. A volume named config-vol is mounted inside the container at the path /etc/config, and this volume is sourced from a ConfigMap named my-config. This setup allows the container to access configuration data stored in the ConfigMap as files within the specified directory." },
             { value: 'configmaps-using-literals', label: 'Configmaps using literals', hint: "This option allows you to generate a lightweight Alpine-based Pod that mounts a specific folder containing configuration files provided through ConfigMaps created from literal values. It’s particularly useful for testing or deploying minimal containers where configuration data needs to be injected at runtime in a structured and accessible way within the container’s filesystem." },
             { value: 'pod-alpine-using-variable-environments', label: 'Pod Alpine using configmap to charge environment variables', hint: "This configuration defines a lightweight Alpine-based Pod that uses a ConfigMap to load environment variables into the container. By referencing the ConfigMap, the Pod can dynamically inject key-value pairs as environment variables, enabling better separation of configuration from code and allowing easier updates or modifications to runtime settings without changing the container image. This approach is ideal for managing environment-specific configurations in a clean and maintainable way." },
+            { value: 'pod-alpine-using-all-variables-in-the-configmap', label: 'Pod Alpine using all values loaded in the configmap', hint: "This setup defines an Alpine-based Pod that loads all key-value pairs from a ConfigMap as environment variables within the container. By referencing the entire ConfigMap, the Pod can access all its configuration data without specifying each variable individually. This approach streamlines configuration management, especially when dealing with multiple environment variables, and ensures the container has access to all necessary settings defined in the ConfigMap at runtime." },
 
         ],
     });
@@ -22,7 +24,10 @@ export async function run(){
             handler = new ConfigmapsUsingLiteralsHandler(); 
             break;
         case "pod-alpine-using-variable-environments":
-            handler = new PodAlpineUsingEnvironmentVariablesHandler()
+            handler = new PodAlpineUsingEnvironmentVariablesHandler();
+            break;
+        case "pod-alpine-using-all-variables-in-the-configmap":
+            handler = new LoadAllConfigmapsInAlpinePodHandler();
             break;
         default:
             break;
